501a42aa0f21fcc67af95903265739a5172a6be2
saif <sirsaif99@gmail.com>
2022-07-24 11:56:36 +0200

    WIP: fix release.sh

--- release.sh
+++ release.sh
@@ -4,7 +4,10 @@ set -e
 
 # shellcheck disable=SC1083
 IMAGE_ID=$(docker inspect "${HEROKU_REGISTRY_IMAGE}" --format={{.Id}})
+echo IMAGE_ID
+
 PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}]}'
+echo PAYLOAD
 
 curl -n -X PATCH https://api.heroku.com/apps/"$HEROKU_APP_NAME"/formation \
   -d "${PAYLOAD}" \
