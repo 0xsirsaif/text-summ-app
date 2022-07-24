c72724d4d39da0aa2831ea14d054a5a6c757f017
saif <sirsaif99@gmail.com>
2022-07-24 11:30:08 +0200

    WIP: remove --cache-from in the main.yml.

--- release.sh
+++ release.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-IMAGE_ID=$(docker inspect ${HEROKU_REGISTRY_IMAGE} --format={{.Id}})
+# shellcheck disable=SC1083
+IMAGE_ID=$(docker inspect "${HEROKU_REGISTRY_IMAGE}" --format={{.Id}})
 PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}]}'
 
-curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
+curl -n -X PATCH https://api.heroku.com/apps/"$HEROKU_APP_NAME"/formation \
   -d "${PAYLOAD}" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
